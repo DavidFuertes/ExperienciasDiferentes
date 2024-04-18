cfe9670f5e43717582afd9f95869782ec6979acb
Galactic Peanut <jorgeog99cdr@gmail.com>
2024-04-18 16:44:06 +0200

    upload .env

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -3,11 +3,11 @@
 import getPool from "./getPool.js"
 
 const searchTerm = process.argv[2]; // Término de búsqueda
-const DB_NAME = process.env.DB_NAME
+const DB_NAME = process.env.MYSQL_DB
 
 const myPool = getPool();
 
-await myPool.query(`USE ${DB_NAME}`); 
+await myPool.query(`USE ${MYSQL_DB}`); 
 
 const [user] = await myPool.query(`SELECT user.id as id, password FROM users WHERE users LIKE "%${searchTerm}%`) // Ejemplo, esperando términos de búsqueda
 
