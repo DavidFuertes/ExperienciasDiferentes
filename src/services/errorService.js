496f542a1876ea7ff734c0a4a2a722877b0dd6a9
Daniel Moreno <morenoacedo@gmail.com>
2024-04-19 11:34:53 +0200

    añadido src/services/errorService.js

---
+++ src/services/errorService.js
@@ -0,0 +1,11 @@
+//Creación de errores personalizados 
+//Estructura modelo de ejemplo:
+/* export const nombreDelError = () => {
+    throw {
+        httpStatus: XXX,
+        code: 'nombre código para nosotros',
+        message: 'Mensaje para el cliente',
+    };
+}; */
+
+
