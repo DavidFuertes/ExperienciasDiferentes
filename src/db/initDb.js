e38c74338533d79cfc9a7237723507267430610a
Dante Fuertes <davidfuertesrojas@gmail.com>
2024-04-22 23:18:09 +0200

    Añadido endpoint de filtrado y base de datos funcional, con datos aleatorios rellenos en las tablas para poder trabajar con ellas.

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -3,53 +3,43 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-async function insertSampleData() {
-    let connection;
+async function insertSampleData(connection) {
     try {
-        // Conectamos al pool
-        connection = await pool.getConnection();
-
         // Metemos datos en la tabla Users
         await connection.query(`
-      INSERT INTO Users (name, email, password, date, avatar, registrationCode, active, role)
-      VALUES
-        ('Juan Pérez', 'juanperez@example.com', 'contraseña123', '1990-01-01', 'avatar1.jpg', 'ABC123', true, 'admin'),
-        ('María García', 'mariagarcia@example.com', 'contraseña456', '1995-03-15', 'avatar2.jpg', 'DEF456', true, 'público')
-    `);
+            INSERT INTO Users (name, email, password, date, avatar)
+            VALUES
+                ('Juan Pérez', 'juanperez@example.com', 'contraseña123', '1990-01-01', 'avatar1.jpg'  ),
+                ('María García', 'mariagarcia@example.com', 'contraseña456', '1995-03-15', 'avatar2.jpg' )
+        `);
 
         //  Metemos datos en la tabla Experiences
         await connection.query(`
-      INSERT INTO Experiences (creator_id, title, description, type, city, image, date, price, min_places, total_places, active)
-      VALUES
-        (1, 'Aventura en las Montañas', 'Experimenta la emoción de hacer senderismo en las montañas.', 'Adrenaline rush', 'Mountainville', 'montaña.jpg', '2024-05-01', 50.00, 5, 10, true),
-        (2, 'Tour por la Ciudad', 'Explora la ciudad y sus puntos de referencia.', 'Relaxed', 'Cityville', 'ciudad.jpg', '2024-06-15', 30.00, 3, 8, true)
-    `);
+            INSERT INTO Experiences (creator_id, title, description, type, city, image, date, price, min_places, total_places, active)
+            VALUES
+                (1, 'Aventura en las Montañas', 'Experimenta la emoción de hacer senderismo en las montañas.', 'Adrenalina pura', 'Mountainville', 'montaña.jpg', '2024-05-01', 50.00, 5, 10, true),
+                (2, 'Tour por la Ciudad', 'Explora la ciudad y sus puntos de referencia.', 'Relajado', 'Cityville', 'ciudad.jpg', '2024-06-15', 30.00, 3, 8, true)
+        `);
 
         // Insert sample data into the Reservations table
         await connection.query(`
-      INSERT INTO Reservations (user_id, experience_id)
-      VALUES
-        (1, 1),
-        (2, 2)
-    `);
+            INSERT INTO Reservations (user_id, experience_id)
+            VALUES
+                (1, 1),
+                (2, 2)
+        `);
 
         // Insert sample data into the Comments table
         await connection.query(`
-      INSERT INTO Comments (user_id, experience_id, content, rate)
-      VALUES
-        (1, 1, '¡Experiencia increíble!', 5),
-        (2, 2, '¡Buen tour, me encantó!', 4)
-    `);
+            INSERT INTO Comments (user_id, experience_id, content, rate)
+            VALUES
+                (1, 1, '¡Experiencia increíble!', 5),
+                (2, 2, '¡Buen tour, me encantó!', 4)
+        `);
 
         console.log('Datos de ejemplo insertados correctamente.');
     } catch (error) {
         console.error('Error al insertar datos de ejemplo:', error.message);
-    } finally {
-        if (connection) {
-            // Liberamos la conexión solo si está abierta
-            connection.release();
-        }
-        // No cerramos la conexión aquí ya que el pool maneja las conexiones
     }
 }
 
@@ -92,57 +82,84 @@ async function createDatabaseAndTables() {
         // Usamos la base de datos recién creada
         await connection.query(`USE ${process.env.MYSQL_DB}`);
 
-        // Ejecutamos las consultas SQL para crear las tablas
-        await connection.query(`
-      -- Crear tabla Users
-      CREATE TABLE IF NOT EXISTS Users (
-          user_id INT AUTO_INCREMENT PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          email VARCHAR(255) NOT NULL,
-          password VARCHAR(255) NOT NULL,
-          date_of_birth DATE,
-          avatar VARCHAR(255),
-          registration_code VARCHAR(255),
-          active BOOLEAN DEFAULT false,
-          role ENUM('admin', 'public') NOT NULL DEFAULT 'public'
-      );
-
-      -- Crear tabla Experiences
-        CREATE TABLE IF NOT EXISTS Experiences (
-            experience_id INT AUTO_INCREMENT PRIMARY KEY,
-            creator_id INT,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            type ENUM('Adrenaline rush', 'Relaxed') NOT NULL,
-            city VARCHAR(255),
-            image VARCHAR(255),
-            date DATE,
-            price DECIMAL(10, 2),
-            min_places INT,
-            total_places INT,
-            active BOOLEAN DEFAULT true,
-            FOREIGN KEY (creator_id) REFERENCES Users(id)
-            );
-
-      -- Crear tabla Reservations
-      CREATE TABLE IF NOT EXISTS Reservations (
-          reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-          user_id INT,
-          experience_id INT,
-          reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          FOREIGN KEY (user_id) REFERENCES Users(id),
-          FOREIGN KEY (experience_id) REFERENCES Experiences(id)
-      );
-    `);
+        // Creamos las tablas individualmente
+        await createTables(connection);
 
         // Introducimos datos de ejemplo
-        await insertSampleData();
+        await insertSampleData(connection);
     } catch (error) {
         console.error('Error creating database and tables:', error.message);
     } finally {
         if (connection) {
             connection.release();
         }
+    }
+}
+
+async function createTables(connection) {
+    try {
+        // Creamos la tabla Usuarios
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS Users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                date DATE,
+                avatar VARCHAR(255),
+                register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+
+        // Creamos la tabla Experiencias
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS Experiences (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                creator_id INT,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                type ENUM('Relajado', 'Medio', 'Adrenalina pura') NOT NULL,
+                city VARCHAR(255),
+                image VARCHAR(255),
+                date DATE,
+                price DECIMAL(10, 2),
+                min_places INT,
+                total_places INT,
+                active BOOLEAN DEFAULT true,
+                creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (creator_id) REFERENCES Users(id)
+            )
+        `);
+
+        // Creamos la tabla Reservas
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS Reservations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT,
+                experience_id INT,
+                reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES Users(id),
+                FOREIGN KEY (experience_id) REFERENCES Experiences(id)
+            )
+        `);
+
+        // Creamos la tabla Comentarios
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS Comments (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT,
+                experience_id INT,
+                content TEXT,
+                rate INT CHECK(rate >= 0 AND rate <= 5),
+                comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES Users(id),
+                FOREIGN KEY (experience_id) REFERENCES Experiences(id)
+            )
+        `);
+
+        console.log('Tablas creadas exitosamente.');
+    } catch (error) {
+        console.error('Error al crear las tablas:', error.message);
     }
 }
 
