2cfa7542b7f75f0f4a8647e04031030ea7caa671
xevia <xeviarenasarafael@gmail.com>
2024-04-23 21:15:45 +0200

    app.js y arreglo database

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-//Importamos las dependencias necesarias
+// importamos express para crear el servidor
 import express from 'express';
-import 'dotenv/config.js';
-// Obtenemos las variables de entorno.
-const PORT = process.env.PORT || 3000;
+import fileUpload from 'express-fileupload';
 
-//Creación del servidor con express
-const app = express();
+// Importamos morgan para mostrar información de las peticiones
+import morgan from 'morgan';
 
-//Arrancamos la BBDD
-import initDb from './src/db/getPool.js';
+// Importamos cors para evitar problemas con las CORS
+//import cors from 'cors';
 
-// Le indicamos al servidor que escuche peticiones en el puerto establecido en
-// las variables de entorno.
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+// Importamos las rutas
+import { routes } from './src/routes/index.js';
+
+// Importamos los controladores de errores
+import {
+  notFoundController,
+  errorController,
+} from './src/controllers/errors/index.js';
+
+// Obtenemos las variables de entorno
+import { PORT } from './env.js';
+
+//const db = getPool();
+const app = express(); // crea servidor
+
+const appPORT = PORT || 3000;
+
+app.use(express.json()); // Milddeware para parsear el body
+app.use(fileUpload());
+
+app.use(morgan('dev')); // Milddewar de morgan
+
+app.use(routes); // Milddewar que indica a express donde estan las rutas cuando esten hechas
+
+app.use(notFoundController); // Milddewar de ruta no encontrada
+
+app.use(errorController); //Milddewar de Error
+
+app.listen(appPORT, () => {
+  console.log(`🚀 Servidor escuchando en http://localhost:${appPORT}`);
 });
--- src/services/errorService.js
+++ src/services/errorService.js
@@ -7,3 +7,43 @@ export const notFoundError = (resource) => {
     };
 };
 
+
+export const userAlreadyRegisteredError = () => {
+  throw {
+    httpStatus: 409, // Conflict
+    code: 'USER_ALREADY_REGISTERED',
+    message: 'El nombre de usuario ya está registrado',
+  };
+};
+
+export const emailAlreadyRegisteredError = () => {
+  throw {
+    httpStatus: 409, // Conflict
+    code: 'EMAIL_ALREADY_REGISTERED',
+    message: 'El email ya está registrado',
+  };
+};
+
+export const sendEmailError = () => {
+  throw {
+    httpStatus: 500, // Internal server Error
+    code: 'SEND_EMAIL_REGISTERED',
+    message: 'Error al enviar email',
+  };
+};
+
+export const userNotExistError = () => {
+  throw {
+    httpStatus: 409, // Conflict
+    code: 'USERNAME_NOT_EXIST',
+    message: 'El usuario no existe',
+  };
+};
+
+export const userNotValidPasswordError = () => {
+  throw {
+    httpStatus: 409, // Conflict
+    code: 'PASSWORD_NOT_VALID',
+    message: 'La constraseña no es válida',
+  };
+};
