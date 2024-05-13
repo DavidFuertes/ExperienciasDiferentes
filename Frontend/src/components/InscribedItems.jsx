2b5c3ac3fd5b6fc22760283d373e435e72033f22
DanielEireos <danieireos2002@gmail.com>
2024-05-13 16:44:59 +0200

    modificación InscribedItem

--- Frontend/src/components/InscribedItems.jsx
+++ Frontend/src/components/InscribedItems.jsx
@@ -1,18 +1,15 @@
-function InscribedItems ({inscribed}) {
+function InscribedItems({ inscribed }) {
+  const { id, name, email, avatar } = inscribed.inscribed;
 
-    const {id, name, email, avatar} = inscribed
-
-    console.log(inscribed)
-    return (
-        <tr>
-            <td>{avatar}</td>
-            <td>{name}</td>
-            <td>{email}</td>
-            <td>{id}</td>
-        </tr>
-
-    )
-
+  console.log(inscribed);
+  return (
+    <tr>
+      {/* <td>{avatar}</td> */}
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>{id}</td>
+    </tr>
+  );
 }
 
 export default InscribedItems;
