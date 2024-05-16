99ee7b90488fc082b8da32c5a18d576cc055d7d7
DanielEireos <danieireos2002@gmail.com>
2024-05-17 01:18:12 +0200

    prueba con imagen de usuario

--- Frontend/src/components/UserAuth.jsx
+++ Frontend/src/components/UserAuth.jsx
@@ -5,9 +5,14 @@ import { UserContext } from "../context/UserContext.jsx";
 export const UserAuth = () => {
   const { user, logout } = useContext(UserContext);
 
+  const avatarUrl = user
+    ? URL.createObjectURL(new Blob([user.user.avatar]))
+    : null;
+
   return user ? (
     <section>
       Bienvenido <Link to={`/account`}>{user.user.name}</Link>{" "}
+      <img src={avatarUrl}></img>
       {user.user.role === "admin" && (
         <Link to={`/experienceadministration`}>
           <button>Control Panel</button>
