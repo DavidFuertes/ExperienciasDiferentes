b6f169dfbbea405111035d06cdaa15d9790c04a1
DanielEireos <danieireos2002@gmail.com>
2024-05-17 15:07:23 +0200

    pruebas con avatar del usuario

--- Frontend/src/components/UserAuth.jsx
+++ Frontend/src/components/UserAuth.jsx
@@ -4,11 +4,31 @@ import { UserContext } from "../context/UserContext.jsx";
 
 export const UserAuth = () => {
   const { user, logout } = useContext(UserContext);
+  console.log("user", user);
+
+  const renderAvatar = () => {
+    if (user && user.user.avatar.data) {
+      const byteArray = user.user.avatar.data;
+      const base64String = btoa(
+        String.fromCharCode(...new Uint8Array(byteArray))
+      );
+      const avatarUrl = `data:image/jpeg;base64,${base64String}`;
+      console.log("avatarUrl", avatarUrl);
+      return (
+        <img
+          src={avatarUrl}
+          alt="avatar"
+          style={{ maxWidth: "200px", maxHeight: "200px" }}
+        />
+      );
+    }
+    return null;
+  };
 
   return user ? (
     <section>
-      Bienvenido <Link to={`/account`}>{user.user.name}</Link>{" "}
-      <img alt="avatar"></img>
+      Bienvenido <Link to={`/account`}>{user.user.name}</Link>
+      {renderAvatar()}
       {user.user.role === "admin" && (
         <Link to={`/experienceadministration`}>
           <button>Control Panel</button>
